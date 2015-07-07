9b67945ca006bfc2f415d63649b651317edc8c24
Vlad Nicula <vladnicula@gmail.com>
2015-07-07 19:05:58 +0300

    Star canvas basic component

--- src/components/value-slider/index.js
+++ src/components/value-slider/index.js
@@ -1,6 +1,10 @@
+var NumberInput = require("/src/components/number-input");
+
 function ValueSlider ( Mediator ) {
 	this.mediator = Mediator;
 	this.el = document.createElement("div");
+	this.numberInput = new NumberInput();
+	this.el.appendChild(this.numberInput.getDOMNode());
 }
 
 ValueSlider.prototype.getDOMNode = function () {
